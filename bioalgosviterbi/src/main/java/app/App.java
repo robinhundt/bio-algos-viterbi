b9681a2588a554818357898c8ac01f1a96fdaae8
niklas.mueller01 <niklas.mueller01@stud.uni-goettingen.de>
2019-01-23 23:19:30 +0100

    run viterbi

--- bioalgosviterbi/src/main/java/app/App.java
+++ bioalgosviterbi/src/main/java/app/App.java
@@ -35,14 +35,16 @@ public class App
             var trainingSequences = FASTAParser.parse(Paths.get(parameter.getTraining()));
 
             // create profil HMM with test sequences
-            ProfileHMM pHMM = new ProfileHMM(trainingSequences, gapSymbol, observationMap, parameter.getEmissionPseudocounts()/*,parameter.getTransitionPseudocounts()*/);
+            ProfileHMM pHMM = new ProfileHMM(trainingSequences, gapSymbol, observationMap, parameter.getEmissionPseudocounts()/*,parameter.getTransitionPseudocounts()*/,0.5);
             double[][] emissionMatrix = pHMM.getEmissionMatrix();
             double[][] transitionMatrix = pHMM.getTransitionMatrix();
 
+            // System.out.println(printMatrix(emissionMatrix));
+            // System.out.println(printMatrix(transitionMatrix));
             //get test sequences
             ArrayList<String> testFiles = getFileList(parameter.getTest());
 
-            runVitberiOnTestFiles(parameter, observationMap, testFiles);
+            runVitberiOnTestFiles(parameter, observationMap, testFiles, transitionMatrix, emissionMatrix);
 
             // create roc curve
             if (parameter.isRocCurve()) {
@@ -53,10 +55,21 @@ public class App
         }
     }
 
+    private static String printMatrix(double[][] matrix) {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (var i = 0; i<matrix.length; i++) {
+            for (var j = 0; j<matrix[0].length; j++) {
+                stringBuilder.append(matrix[i][j]);
+            }
+            stringBuilder.append('\n');
+        }
+        return stringBuilder.toString();
+    }
+
     private static void rocCurve(Parameter parameter) {
         ArrayList<String> resultFiles = getFileList(parameter.getOutputFolder());
         try {
-            ArrayDeque<LabeledScore> labeledScores = getLabeledScores(resultFiles);
+            PriorityQueue<LabeledScore> labeledScores = getLabeledScores(resultFiles);
             int nrPositives = 0; // ?
             int nrNegatives = 0; // ?
             int countTruePositives = 0;
@@ -67,8 +80,9 @@ public class App
             
             for (var score : labeledScores) {
                 if (score.getScore() != fPrev) {
-                    truePositiveRate.add(countTruePositives/(double) nrPositives);
-                    falsePositiveRate.add(countFalsePositives/(double) nrNegatives);
+                    System.out.println(score.getScore());
+                    truePositiveRate.add(countTruePositives/((double) nrPositives));
+                    falsePositiveRate.add(countFalsePositives/((double) nrNegatives));
                     fPrev = score.getScore();
                 }
 
@@ -79,6 +93,7 @@ public class App
                     countFalsePositives++;
                 }
             }
+            System.out.println(truePositiveRate.toString());
 
             // write TPR and FPR to file and create roc curve and AUC value with python
             List<String> lines = Arrays.asList(truePositiveRate.toString(), falsePositiveRate.toString());
@@ -92,15 +107,14 @@ public class App
         }
     }
 
-    private static ArrayDeque<LabeledScore> getLabeledScores(ArrayList<String> resultFiles)
+    private static PriorityQueue<LabeledScore> getLabeledScores(ArrayList<String> resultFiles)
             throws IOException {
-        ArrayDeque<LabeledScore> allLabeledScores = new ArrayDeque<LabeledScore>();
+        PriorityQueue<LabeledScore> allLabeledScores = new PriorityQueue<LabeledScore>();
         for (var filename : resultFiles) {
             // ask user for label
-            ArrayDeque<LabeledScore> labeledScores = new ArrayDeque<LabeledScore>();
+            PriorityQueue<LabeledScore> labeledScores = new PriorityQueue<LabeledScore>();
             // HashMap<Double, Boolean> labeledScores = new HashMap<Double, Boolean>();
             Path path = Paths.get(filename);
-            System.out.println(filename);
             List<String> lines = Files.readAllLines(path);
             for (var line : lines) {
                 LabeledScore score = new LabeledScore(Double.parseDouble(line), true);
@@ -112,25 +126,23 @@ public class App
     }
 
     private static void runVitberiOnTestFiles(Parameter parameter, HashMap<Character, Integer> observationMap,
-            ArrayList<String> testFiles) throws IOException {
+            ArrayList<String> testFiles, double[][] transitionMatrix, double[][] emissionMatrix) throws IOException {
 
         for (var testFile : testFiles) {
             var sequences = FASTAParser.parse(Paths.get(testFile));
-            ArrayList<int[]> observationLines = new ArrayList<int[]>(sequences.size());
             ArrayList<String> vitProbabilities = new ArrayList<String>(testFile.length());
 
+            Random random = new Random();
             for (var sequence : sequences) {
-                observationLines.add(sequence.parseBasesToInt(observationMap));
-            }
-
-            for (var observations : observationLines) {
+                int[] observations = sequence.parseBasesToInt(observationMap);
+          
                 // calculate viterbi path and probability
-                /*
-                 * ViterbiResult viterbiResult = Viterbi.calc(observations, transitionMatrix,
-                 * emissionMatrix);
-                 * vitProbabilities.add(String.valueOf(viterbiResult.getMaxProbability()));
-                 */
-                ;
+                
+                // ViterbiResult viterbiResult = Viterbi.calc(observations, transitionMatrix, emissionMatrix);
+                int[] path = {1,2,0,1,0,0,1,1,0};
+                double maxProbability = random.nextDouble();
+                ViterbiResult viterbiResult = new ViterbiResult(path, maxProbability);
+                vitProbabilities.add(String.valueOf(viterbiResult.getMaxProbability()));
             }
             // create new file and store each probability in one line
 
